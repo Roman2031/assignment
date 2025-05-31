55d0d227a0ff669b66441d57b8bd81a3a49e66d7
Abdullah Al Roman <abdullahr583@live.com>
2025-05-31 13:17:12 +0600

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:assignment/auth/auth_service.dart';
+import 'package:assignment/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = AuthService();
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome User",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  goToLogin(BuildContext context) => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+}
